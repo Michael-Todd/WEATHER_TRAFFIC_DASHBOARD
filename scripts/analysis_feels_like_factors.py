ae36b3ed7cc5e782bc14e082d06b9c85704e2f9b
Michael Camp <mcam320@wgu.edu>
2026-02-14 11:22:10 -0600

    adds initial version of analysis_feels_like_factors

---
+++ scripts/analysis_feels_like_factors.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import mysql.connector
+import os
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+conn = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
+
+query = """
+SELECT
+city,
+date,
+current_temperature,
+feels_like_temperature,
+humidity,
+wind_speed
+FROM weather_data
+ORDER BY city, date
+"""
+
+df = pd.read_sql(query, conn)
+conn.close()
+
+df["date"] = pd.to_datetime(df["date"])
+df["temperature_difference"] = df["current_temperature"] - df["feels_like_temperature"]
+
+df.to_csv("data/feels_like_factors.csv", index = False)
